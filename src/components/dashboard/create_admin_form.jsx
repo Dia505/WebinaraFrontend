899f56f442ff7c7ad522798b90febe512e026b69
Dia505 <dia48261@gmail.com>
2025-07-16 02:18:36 +0545

    Admin dashboard + create admin form

---
+++ src/components/dashboard/create_admin_form.jsx
@@ -0,0 +1,145 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useForm } from 'react-hook-form';
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import { useAuth } from '../../context/auth_context';
+
+import "../css_files/dashboard/create_admin_form.css";
+
+const adminSchema = yup.object().shape({
+    email: yup.string().email("Invalid email").required("*required"),
+    password: yup
+        .string()
+        .min(8, "Password must be at least 8 characters")
+        .max(16, "Password cannot exceed 16 characters")
+        .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+        .matches(/[0-9]/, "Password must contain at least one number")
+        .matches(/[@$!%*?&#]/, "Password must contain at least one special character")
+        .required("*required"),
+    confirmPassword: yup
+        .string()
+        .required("*required")
+        .oneOf([yup.ref("password"), null], "Passwords must match")
+});
+
+function CreateAdminForm({ closeForm }) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setError,
+    } = useForm({
+        resolver: yupResolver(adminSchema),
+        mode: "all",
+    });
+
+    const { authToken } = useAuth();
+
+    const registerAdmin = useMutation({
+        mutationKey: "SAVEDATA",
+        mutationFn: (requestData) => {
+            console.log(requestData);
+            return axios.post("http://localhost:3000/api/auth/register",
+                requestData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${authToken}`
+                    }
+                }
+            );
+        },
+        onSuccess: (data) => {
+            toast.success("Admin successfully created!", {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                theme: "colored",
+            });
+            closeForm();
+        },
+        onError: (error) => {
+            console.error("Error registering new admin:", error);
+
+            const errData = error.response?.data;
+
+            if (error.response?.status === 403 && errData?.field && errData?.message) {
+                setError(errData.field, { type: "manual", message: errData.message });
+                return;
+            }
+
+            toast.error("Failed to register new admin. Please try again.", {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                theme: "colored",
+            });
+        },
+    });
+
+    const onSubmit = (values) => {
+        const { email, password } = values;
+        registerAdmin.mutate({ email, password });
+    };
+
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="create-admin-form-main-div">
+                    <div className="create-admin-form-title-btn-div">
+                        <p>Create new admin</p>
+                        <p onClick={closeForm}>X</p>
+                    </div>
+
+                    <div>
+                        <p className='create-admin-input-title'>Email address</p>
+                        <input
+                            type='text'
+                            {...register("email")}
+                            className={errors.email ? "input-error" : ""}
+                        />
+                        {errors.email && <p className="error-message">{errors.email.message}</p>}
+                    </div>
+
+                    <div>
+                        <p className='create-admin-input-title'>Password</p>
+                        <input
+                            type='password'
+                            {...register("password")}
+                            className={errors.password ? "input-error" : ""}
+                        />
+                        {errors.password && <p className="error-message">{errors.password.message}</p>}
+                    </div>
+
+                    <div>
+                        <p className='create-admin-input-title'>Confirm password</p>
+                        <input
+                            type='password'
+                            {...register("confirmPassword")}
+                            className={errors.confirmPassword ? "input-error" : ""}
+                        />
+                        {errors.confirmPassword && <p className="error-message">{errors.confirmPassword.message}</p>}
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="create-admin-form-btn"
+                    >
+                        Create admin
+                    </button>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default CreateAdminForm;
--- src/core/private/admin/dashboard.jsx
+++ src/core/private/admin/dashboard.jsx
@@ -1,12 +1,124 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../../../context/auth_context";
+import axios from "axios";
+
 import AdminSideBar from "../../../components/navigation/admin_side_bar";
+import CreateAdminForm from "../../../components/dashboard/create_admin_form";
 import "../../css_files/private/admin/dashboard.css";
 
 function Dashboard() {
-    return(
+    const [users, setUsers] = useState([]);
+    const [userCount, setUserCount] = useState(0);
+    const [webinarCount, setWebinarCount] = useState(0);
+    const { authToken } = useAuth();
+    const [showCreateAdminForm, setShowCreateAdminForm] = useState(false);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/api/user`, {
+                    headers: {
+                        Authorization: `Bearer ${authToken}`
+                    }
+                });
+                setUsers(response.data);
+                setUserCount(response.data.length);
+            } catch (error) {
+                console.error("Failed to fetch users:", error);
+            }
+        };
+
+        fetchUsers();
+    }, [authToken]);
+
+    useEffect(() => {
+        const fetchWebinars = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/api/webinar`, {
+                    headers: {
+                        Authorization: `Bearer ${authToken}`
+                    }
+                });
+
+                setWebinarCount(response.data.length);
+            } catch (error) {
+                console.error("Failed to fetch webinars:", error);
+            }
+        };
+
+        fetchWebinars();
+    }, [authToken]);
+
+    return (
         <>
             <div className="dashboard-main-window">
                 <div className="admin-side-bar-div">
-                    <AdminSideBar/>
+                    <AdminSideBar />
+                </div>
+
+                <div className="dashboard-main-div">
+                    <p className="dashboard-title">Dashboard</p>
+
+                    <div className="dashboard-create-admin-btn-div">
+                        <p className="dashboard-welcome-back-text">Welcome back👋</p>
+                        <button className="dashboard-create-admin-btn" onClick={() => setShowCreateAdminForm(true)}>Create new admin +</button>
+                    </div>
+
+                    <div className="dashboard-insights-div">
+                        <div className="dashboard-insight">
+                            <p className="dashboard-insight-title">Total users</p>
+                            <p className="dashboard-insight-stats">{userCount}</p>
+                        </div>
+                        <div className="dashboard-insight">
+                            <p className="dashboard-insight-title">Total active webinars</p>
+                            <p className="dashboard-insight-stats">{webinarCount}</p>
+                        </div>
+                    </div>
+
+                    <div className="dashboard-table-wrapper">
+                        <table className="dashboard-table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Mobile</th>
+                                    <th>Email</th>
+                                    <th>Address</th>
+                                    <th>City</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.map((user, index) => {
+                                    return (
+                                        <tr key={user?._id}>
+                                            <td>{index + 1}</td>
+                                            <td style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+                                                <img
+                                                    src={user.profilePicture}
+                                                    alt="profile"
+                                                    className="dashboard-table-user-img"
+                                                />
+                                                {user.fullName}
+                                            </td>
+                                            <td>{user.mobileNumber}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.address}</td>
+                                            <td>{user.city}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {showCreateAdminForm && (
+                        <>
+                            <div className="dashboard-overlay" onClick={() => setShowCreateAdminForm(false)}></div>
+                            <div className="dashboard-form-modal">
+                                <CreateAdminForm closeForm={() => setShowCreateAdminForm(false)} />
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </>
